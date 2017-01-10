b436cc8ebcfe4f2d5741ac95b314f6b2e5d62066
jnakaoka <johannnakaoka_7@hotmail.com>
2017-01-10 17:10:43 -0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { ProdutosModule } from './produtos/produtos.module';
+import { SharedModule } from './_shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
+
+    
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    SharedModule,
+    AppRoutingModule
+    ,ProdutosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
